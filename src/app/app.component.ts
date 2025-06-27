1a8d7ef4c9499b7b3bf469cae354f84312b87edf
Kareem-Mohamed-Farouh <kimofro@gmail.com>
2025-06-27 03:07:35 +0300

    feat: add HomeComponent with slider and product display
    - Implemented HomeComponent with a slider and product cards.
    - Created MainsliderComponent for the main slider functionality.
    - Added AddbuttonComponent for adding products to the cart.
    - Designed product cards with images, discounts, and reviews.
    - Included responsive design for various screen sizes.
    - Added unit tests for HomeComponent, MainsliderComponent, and AddbuttonComponent.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './layout/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AddbuttonComponent } from './shared/components/ui/addbutton/addbutton.component';
 import { SliderComponent } from './shared/components/ui/slider/slider.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SliderComponent],
+  imports: [
+    RouterOutlet,
+    SliderComponent,
+    AddbuttonComponent,
+    HomeComponent,
+    FooterComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
---
+++ src/app/shared/components/ui/mainslider/mainslider.component.ts
@@ -0,0 +1,10 @@
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+@Component({
+  selector: 'app-mainslider',
+  imports: [],
+  templateUrl: './mainslider.component.html',
+  styleUrl: './mainslider.component.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+})
+export class MainsliderComponent {}
