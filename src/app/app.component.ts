f1737960447741c6679f3fd43e0963e1b414e46c
mohamed mostafa elgammal <melgammal497@gmail.com>
2025-06-28 01:18:58 +0300

    contact

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ContanetComponent } from "./pages/contanet/contanet.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ContanetComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
