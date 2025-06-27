e98c277931c06066af59ef06650afdf11e927f61
Kareem-Mohamed-Farouh <kimofro@gmail.com>
2025-06-27 20:24:15 +0300

    Make slider responsive to screen width
    Adjusts the number of visible slides based on window size for improved responsiveness and user experience on different devices. Updates layout classes and removes unused pagination styles.

--- src/app/shared/components/ui/mainslider/mainslider.component.ts
+++ src/app/shared/components/ui/mainslider/mainslider.component.ts
@@ -1,4 +1,12 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import {
+  Component,
+  CUSTOM_ELEMENTS_SCHEMA,
+  HostListener,
+  inject,
+  PLATFORM_ID,
+} from '@angular/core';
+import { SwiperOptions } from 'swiper/types';
 
 @Component({
   selector: 'app-mainslider',
@@ -7,4 +15,28 @@ import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
   styleUrl: './mainslider.component.scss',
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class MainsliderComponent {}
+export class MainsliderComponent {
+  slidesPerView: number = 5;
+  screenWidth!: number;
+
+  private readonly pLATFORM_ID = inject(PLATFORM_ID);
+  ngOnInit() {
+    if (isPlatformBrowser(this.pLATFORM_ID)) {
+      this.getScreenWidth();
+    }
+  }
+
+  @HostListener('window:resize')
+  getScreenWidth() {
+    this.screenWidth = window.innerWidth;
+    if (this.screenWidth >= 120 && this.screenWidth <= 480) {
+      this.slidesPerView = 1;
+    } else if (this.screenWidth >= 480 && this.screenWidth <= 602) {
+      this.slidesPerView = 2;
+    } else if (this.screenWidth >= 602 && this.screenWidth <= 992) {
+      this.slidesPerView = 3;
+    } else if (this.screenWidth >= 992 && this.screenWidth <= 1200) {
+      this.slidesPerView = 5;
+    }
+  }
+}
