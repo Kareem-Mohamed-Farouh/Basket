a405cbf4477bbbdf93dfd8e76ae5064234c36709
Kareem-Mohamed-Farouh <kimofro@gmail.com>
2025-06-28 18:34:35 +0300

    Enables scroll position restoration to top on navigation
    Adds in-memory scroll handling to ensure users are returned to the top of the page when navigating. Improves user experience by providing consistent scroll behavior across route changes.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,26 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import {
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
+import {
+  provideHttpClient,
+  withFetch,
+  withInterceptors,
+} from '@angular/common/http';
 import { headerInterceptor } from './shared/interceptors/header/header.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay()),
-      provideHttpClient(withFetch() , withInterceptors([headerInterceptor])),
-  ]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(
+      routes,
+      withInMemoryScrolling({ scrollPositionRestoration: 'top' })
+    ),
+    provideClientHydration(withEventReplay()),
+    provideHttpClient(withFetch(), withInterceptors([headerInterceptor])),
+  ],
 };
