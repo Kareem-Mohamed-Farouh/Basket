f90afbc55fe00c37b2f4e9f4b9ea40a9215e9dad
Kareem-Mohamed-Farouh <kimofro@gmail.com>
2025-07-05 22:12:02 +0300

    hndle error interceptor

---
+++ src/app/shared/interceptors/handlEerror/handl-eerror.interceptor.ts
@@ -0,0 +1,25 @@
+import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
+import { error } from 'console';
+import { catchError, throwError } from 'rxjs';
+
+import { inject } from '@angular/core';
+import { NotyfService } from '../../../core/services/notyfSer/notyf.service';
+
+export const handlEerrorInterceptor: HttpInterceptorFn = (req, next) => {
+  const notyfService = inject(NotyfService);
+  return next(req).pipe(
+    catchError((err) => {
+      if (
+        err.error.message !==
+        'You are not logged in. Please login to get access'
+      ) {
+        notyfService.warning(err.error.message);
+      } else if (localStorage.getItem('basketToken') === undefined) {
+        notyfService.warning('oky');
+      }
+      return throwError(() => {
+        return err;
+      });
+    })
+  );
+};
