48018981e38d411f037e9ef653f37ddbd3592b19
Kareem-Mohamed-Farouh <kimofro@gmail.com>
2025-07-05 19:15:56 +0300

    Adds server-side route and fixes Inter font import
    Enables server rendering for detail pages by adding a new dynamic route.
    Corrects the Inter font import to ensure proper font loading and consistency.

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,6 +1,8 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+  { path: 'details/:detailsId', renderMode: RenderMode.Server },
+
   {
     path: '**',
     renderMode: RenderMode.Prerender,
