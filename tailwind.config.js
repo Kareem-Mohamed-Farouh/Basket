d9be4484fd7b898cd978f1a2c8cb01a3239e3778
Kareem-Mohamed-Farouh <kimofro@gmail.com>
2025-07-09 01:56:02 +0300

    no change

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,17 +5,15 @@ module.exports = {
         "./src/**/*.{html,js}", // ودي هي ملفات المشروع
     ],
     theme: {
-        extend: {
-
-            colors: {
-                main: 'rgba(53, 175, 160, 1)',
-                stock: '#00b853',
-                addbtn: '#ffcd00',
+        theme: {
+            extend: {
+                colors: {
+                    main: 'rgba(53, 175, 160, 1)',
+                    stock: '#00b853',
+                    addbtn: '#ffcd00',
+                }
             }
-
-
-
-        },
+        }
     },
     plugins: [
         require('flowbite/plugin'), // إضافة البلوجين نفسه
