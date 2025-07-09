9d04e08ca5ab1fbb361e3a6e817dd620d7eb11bd
Kareem-Mohamed-Farouh <kimofro@gmail.com>
2025-07-09 15:15:45 +0300

    ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component, inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './layout/footer/footer.component';
 
-
 import { NgxSpinnerComponent } from 'ngx-spinner';
+import { FlowbiteService } from './core/services/flowbite/flowbite.service';
 
 @Component({
   selector: 'app-root',
@@ -21,4 +21,10 @@ import { NgxSpinnerComponent } from 'ngx-spinner';
 })
 export class AppComponent {
   title = 'eBasket';
+
+  constructor(private flowbiteService: FlowbiteService) {}
+
+  ngOnInit(): void {
+    this.flowbiteService.loadFlowbite((flowbite) => {});
+  }
 }
