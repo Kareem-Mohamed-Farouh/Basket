70677fed4fa93525cd7eb660b86744bc9953c8ea
Kareem-Mohamed-Farouh <kimofro@gmail.com>
2025-07-09 15:29:28 +0300

    edit in  handeler error

--- src/app/shared/interceptors/handlEerror/handl-eerror.interceptor.ts
+++ src/app/shared/interceptors/handlEerror/handl-eerror.interceptor.ts
@@ -9,13 +9,8 @@ export const handlEerrorInterceptor: HttpInterceptorFn = (req, next) => {
   const notyfService = inject(NotyfService);
   return next(req).pipe(
     catchError((err) => {
-      if (
-        err.error.message == 'You are not logged in. Please login to get access'
-      ) {
-        notyfService.warning(err.error.message);
-      }
       return throwError(() => {
-        return err;
+        return notyfService.warning(err.error.message);
       });
     })
   );
